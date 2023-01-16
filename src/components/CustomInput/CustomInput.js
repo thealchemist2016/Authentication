27d5a222118d2f1a7ade43c3f68b28c5bd776aa6
Michael Byrd <michaelbyrd7741@gmail.com>
2023-01-16 02:41:04 -0600

    added custom input module

---
+++ src/components/CustomInput/CustomInput.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomInput = () => {
+  return (
+    <View>
+      <Text>Custom Input</Text>
+    </View>
+  )
+}
+
+export default CustomInput
