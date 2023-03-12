131b4bafecac643420bcb7135e618bbacd072b54
Michael James Byrd Jr <michaelbyrd7741@gmail.com>
2023-03-12 10:37:09 -0500

    added aws amplify

--- App.tsx
+++ App.tsx
@@ -9,9 +9,15 @@
 
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
-
 import Navigation from './src/navigation';
+// @ts-ignore
+import Amplify from 'aws-amplify';
+// @ts-ignore
+import {withAuthenticator} from 'aws-amplify-react-native';
+// @ts-ignore
+import config from './src.aws-exports';
 
+Amplify.configure(config);
 const App = () => {
   return (
     <SafeAreaView style={styles.root}>
@@ -27,4 +33,4 @@ const styles = StyleSheet.create({
 });
 
 
-export default App;
+export default withAuthenticator(App);
